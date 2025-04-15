58676cf4c98e36485d19e2f24c74fa843204986c
ssubhashini2004 <subha8077raghavan@gmail.com>
2025-04-15 23:50:30 +0530

    Setup backend for Render deployment

--- server.cjs
+++ server.cjs
@@ -20,6 +20,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/journals", journalRoutes);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
